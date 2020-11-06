87c6aca7ed2f3895981ca29958fa9e9742493293
Charles NDAYISABA <nccharles1@gmail.com>
2020-11-06 22:16:54 +0200

    added bootstrap

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,9 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  Key_Token: "ab2906c88d83d1dcd4d5acff2f4c6a33c20c710a"
 };
-
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
