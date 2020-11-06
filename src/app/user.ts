4bad08e865b73ff6ddcfcf1a219709a62858c909
Charles NDAYISABA <nccharles1@gmail.com>
2020-11-06 22:44:23 +0200

    created a user class

---
+++ src/app/user.ts
@@ -0,0 +1,9 @@
+export class user {
+    created_at: string;
+    avatar_url: any;
+    followers_url: number;
+    following_url: number;
+    repos_url: string;
+    login: number;
+    public_repos: string;
+}
